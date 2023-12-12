f3a879222c353fdcfe9637c50d34230dc3a96f70
Violet Kester <kestervj89@gmail.com>
2023-12-12 07:18:14 -0500

    added portfolio url patterns

---
+++ portfolio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'portfolio'
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    # path('projects/', views.project_list, name='project_list'),
+    path('projects/<slug:slug>', views.project_detail, name='project_detail'),
+]
