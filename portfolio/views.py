6b34e39bc0eb6fd8109ea8031d2af9d846554ab1
Violet Kester <kestervj89@gmail.com>
2023-12-12 06:11:20 -0500

    added homepage, project detail views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .models import Project
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def homepage(request):
+    """
+    Portfolio homepage.
+    """
+
+    return render(request, 'portfolio/homepage.html')
+
+
+def project_detail(request, slug):
+    """
+    Project detail view.
+
+    Parameters:
+        - `slug`: The project's unique slug identifier.
+
+    Context variables:
+       - `project`: The Project object to be displayed.
+    """
+
+    project = get_object_or_404(Project,
+                                status=Project.Status.PUBLISHED,
+                                slug=slug)
+
+    context = {
+        'project': project,
+    }
+
+    return render(request, 'portfolio/project/detail.html', context)
