11e678b7d21a3088c230e8943eaa9c4968fc58d3
Violet Kester <kestervj89@gmail.com>
2023-12-12 07:30:12 -0500

    registered project model for admin page

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # Displayed columns
+    list_display = ['id', 'slug', 'title', 'author', 'publish', 'status']
+    # Column filters
+    list_filter = ['status', 'created', 'publish', 'author']
+    # Fields displayed in the project edit form
+    fields = ['slug', 'title', 'author', 'description',
+              'thumbnail', 'publish', 'status']
+    # Prepopulate slug field based on project title
+    prepopulated_fields = {'slug': ('title',)}
+    # Order columns by status and publish date by default
+    ordering = ['-status', '-publish']
