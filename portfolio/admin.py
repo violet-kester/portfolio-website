784f8759d8cf883a3a87dbb3593cf1fc1e0e7c5c
Violet Kester <kestervj89@gmail.com>
2023-12-12 23:40:04 -0500

    doc string edit

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,15 +3,17 @@ from .models import Project
 
 
 admin.site.register(Project)
+
+
 class ProjectAdmin(admin.ModelAdmin):
     # Displayed columns
     list_display = ['id', 'slug', 'title', 'author', 'publish', 'status']
     # Column filters
     list_filter = ['status', 'created', 'publish', 'author']
-    # Fields displayed in the project edit form
+    # Fields displayed in the admin edit form
     fields = ['slug', 'title', 'author', 'description',
               'thumbnail', 'publish', 'status']
-    # Prepopulate slug field based on project title
+    # Prepopulate slug field based on title
     prepopulated_fields = {'slug': ('title',)}
     # Order columns by status and publish date by default
     ordering = ['-status', '-publish']
