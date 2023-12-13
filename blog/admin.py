afa5f3ef8ecba768b06366bae95e7f160aaff437
Violet Kester <kestervj89@gmail.com>
2023-12-12 23:39:10 -0500

    added blog Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Displayed columns
+    list_display = ['id', 'slug', 'title', 'author', 'publish', 'status']
+    # Column filters
+    list_filter = ['status', 'created', 'publish', 'author']
+    # Fields displayed in the admin edit form
+    fields = ['slug', 'title', 'author', 'body',
+              'thumbnail', 'publish', 'status']
+    # Prepopulate slug field based on title
+    prepopulated_fields = {'slug': ('title',)}
+    # Order columns by status and publish date by default
+    ordering = ['-status', '-publish']
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """A model class that represents a post in a blog."""
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    body = models.TextField()
+    thumbnail = models.ImageField(upload_to='blog/static/img/thumbnails/',
+                                  default='blog/static/img/default_thumbnail.png')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the absolute URL of the post detail view."""
+
+        return reverse('blog:post_detail',
+                       args=[self.slug])
