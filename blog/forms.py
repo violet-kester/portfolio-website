bde2f5da8297eb9bf8e40cf99606c2a8f573492b
Violet Kester <kestervj89@gmail.com>
2023-12-13 02:50:03 -0500

    added SharePostForm, CommentForm

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Comment
+
+
+class SharePostForm(forms.Form):
+    senders_name = forms.CharField(max_length=50, label="Your name")
+    senders_email = forms.EmailField(label="Your email address")
+    recipients_email = forms.EmailField(label="Recipient's email address")
+    message = forms.CharField(label="Add a message",
+                               required=False,
+                               widget=forms.Textarea(attrs={'rows': 5}))
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'body']
+        labels = {'name': 'Your name', 'body': 'Comment'}
