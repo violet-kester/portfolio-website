0110f8ca7b896a39a4e31fc6c024cf0b62204d16
Violet Kester <kestervj89@gmail.com>
2023-12-13 02:49:07 -0500

    added post Comment model

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,23 @@ class Post(models.Model):
 
         return reverse('blog:post_detail',
                        args=[self.slug])
+
+
+class Comment(models.Model):
+    """A model class that represents a comment on a post."""
+
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # for manually deactivating comments using the admin site
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
