e6f443d38f6983a928d4326599a41f84a6ad401c
Violet Kester <kestervj89@gmail.com>
2023-12-14 05:30:03 -0500

    added post search form, templates

--- blog/forms.py
+++ blog/forms.py
@@ -4,11 +4,11 @@ from .models import Comment
 
 class SharePostForm(forms.Form):
     senders_name = forms.CharField(max_length=50, label="Your name")
-    senders_email = forms.EmailField(label="Your email address")
+    # senders_email = forms.EmailField(label="Your email address")
     recipients_email = forms.EmailField(label="Recipient's email address")
     message = forms.CharField(label="Add a message",
-                               required=False,
-                               widget=forms.Textarea(attrs={'rows': 5}))
+                              required=False,
+                              widget=forms.Textarea(attrs={'rows': 5}))
 
 
 class CommentForm(forms.ModelForm):
@@ -16,3 +16,11 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['name', 'body']
         labels = {'name': 'Your name', 'body': 'Comment'}
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Search posts!'
+        }))
