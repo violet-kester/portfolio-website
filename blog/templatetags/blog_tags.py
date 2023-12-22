cd91f3b3c256c7751ad8aabbae13a219bd81ab3c
Violet Kester <kestervj89@gmail.com>
2023-12-22 14:38:02 -0500

    added template tag for all blog tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.db.models.functions import Lower
 from ..models import Post
+from taggit.models import Tag
 
 register = template.Library()
 
@@ -21,6 +23,13 @@ def get_most_commented_posts(count=5):
     return Post.objects.filter(status='PB').annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.simple_tag
+def get_all_post_tags():
+    """Returns a list all post tags."""
+
+    return Tag.objects.all().order_by(Lower('name'))
 
 
 @register.simple_tag
