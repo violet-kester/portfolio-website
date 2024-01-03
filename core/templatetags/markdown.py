87c5dbc9192c6c5b1f1821c4b4b103ddfd20681d
Violet Kester <kestervj89@gmail.com>
2024-01-03 16:53:25 -0500

    added support for syntax highlighting

--- core/templatetags/markdown.py
+++ core/templatetags/markdown.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
 import markdown
+import pygments
 
 register = template.Library()
 
@@ -9,4 +10,6 @@ register = template.Library()
 def markdown_format(text):
     """Converts a text to HTML using markdown syntax."""
 
-    return mark_safe(markdown.markdown(text))
+    return mark_safe(
+        markdown.markdown(text, extensions=['fenced_code', 'codehilite'])
+    )
