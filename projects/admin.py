4ad7e45f6ebe7b1e226bd07f1e4d8eef940321da
Violet Kester <kestervj89@gmail.com>
2024-01-10 22:38:36 -0500

    added inline admin class for project screenshots

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Screenshot
 
 
-admin.site.register(Project)
+class ScreenshotInline(admin.TabularInline):
+    model = Screenshot
+    # Number of screenshots to display by default
+    extra = 1
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -12,8 +15,14 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ['status', 'created', 'publish', 'author']
     # Fields displayed in the admin edit form
     fields = ['slug', 'title', 'author', 'summary', 'description',
-              'thumbnail', 'publish', 'status']
+              'logo', 'publish', 'status']
     # Prepopulate slug field based on title
     prepopulated_fields = {'slug': ('title',)}
     # Order columns by status and publish date by default
     ordering = ['-status', '-publish']
+    # Inline admin classes
+    # Add or edit screenshots from the Project admin page
+    inlines = [ScreenshotInline]
+
+
+admin.site.register(Project, ProjectAdmin)
