c4057d591b75128e3d7e88672f833f98815ef57d
Violet Kester <kestervj89@gmail.com>
2024-01-12 00:39:30 -0500

    added models for project links, technologies

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Project, Screenshot
+from .models import Link, Project, Screenshot, Technology
 
+
+class LinkInline(admin.TabularInline):
+    model = Link
+    # Set the order columns appear in the table
+    fields = ('name', 'url', 'bootstrap_icon')
+    # Number of empty slots to display (default is 3)
+    extra = 1
 
 class ScreenshotInline(admin.TabularInline):
     model = Screenshot
-    # Number of screenshots to display by default
     extra = 1
-
 
 class ProjectAdmin(admin.ModelAdmin):
     # Displayed columns
@@ -15,14 +20,23 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ['status', 'created', 'publish', 'author']
     # Fields displayed in the admin edit form
     fields = ['slug', 'title', 'author', 'summary', 'description',
-              'logo', 'publish', 'status']
+              'technologies', 'logo', 'thumbnail', 'publish', 'status']
     # Prepopulate slug field based on title
     prepopulated_fields = {'slug': ('title',)}
     # Order columns by status and publish date by default
     ordering = ['-status', '-publish']
     # Inline admin classes
     # Add or edit screenshots from the Project admin page
-    inlines = [ScreenshotInline]
+    inlines = [ScreenshotInline, LinkInline]
 
 
 admin.site.register(Project, ProjectAdmin)
+
+class TechnologyAdmin(admin.ModelAdmin):
+    # Displayed columns
+    list_display = ['name', 'category']
+
+admin.site.register(Technology, TechnologyAdmin)
+
+
+
--- projects/models.py
+++ projects/models.py
@@ -1,29 +1,65 @@
 from django.contrib.auth.models import User
+from django.db.models.functions import Lower
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
 
+class Technology(models.Model):
+    """A model class that represents a technology used in a project."""
+
+    CATEGORY_CHOICES = [
+        (1, "Backend framework"),
+        (2, "Frontend framework"),
+        (3, "Databases and persistence"),
+        (4, "CSS framework"),
+        (5, "Testing framework"),
+        (6, "Misc."),
+    ]
+    category = models.IntegerField(choices=CATEGORY_CHOICES, default=5)
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['category', Lower('name')]
+        verbose_name = 'Technology'
+        verbose_name_plural = 'Technologies'
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
-    """A model class that represents a project in a portfolio."""
+    """
+    A model class that represents a project in a portfolio.
+
+    Many-to-many relationship with:
+        - Technologies
+
+    One-to-many relationships with:
+        - Links
+        - Screenshots
+    """
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
-
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='projects')
     summary = models.TextField(blank=True)
     description = models.TextField(blank=True)
+    technologies = models.ManyToManyField(Technology, related_name='projects')
     logo = models.ImageField(upload_to='projects/static/projects/img/logos/',
-                             default='static/img/logos/logo-480.png')
+                             default='static/img/logos/portfolio-logo-480.png')
+    thumbnail = models.ImageField(
+        upload_to='projects/static/projects/img/thumbnails/',
+        default='static/img/logos/portfolio-logo-480.png'
+    )
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -45,8 +81,27 @@ class Project(models.Model):
             'https://example.com/projects/jobly/'
         """
 
-        return reverse('portfolio:project_detail',
+        return reverse('projects:project_detail',
                        args=[self.slug])
+
+
+class Link(models.Model):
+    """A model class that represents a link related to a project."""
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='links'
+    )
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=300)
+    bootstrap_icon = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.project.title} link to {self.name}'
 
 
 class Screenshot(models.Model):
@@ -62,4 +117,4 @@ class Screenshot(models.Model):
     )
 
     def __str__(self):
-        return f'Screenshot of {self.project.title}'
+        return f'{self.project.title} screenshot {self.id}'
