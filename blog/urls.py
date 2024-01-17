5f70b0075a7842760891d7f96206234f277602b9
Violet Kester <kestervj89@gmail.com>
2024-01-17 15:05:36 -0500

    moved post search url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,11 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('posts/', views.post_list, name='post_list'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('search/', views.post_search, name='post_search'),
+    path('<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('posts/<slug:post_slug>', views.post_detail, name='post_detail'),
     path('posts/<slug:post_slug>/share/',
          views.post_share, name='post_share'),
     path('posts/<slug:post_slug>/comment/',
          views.post_comment, name='post_comment'),
-    path('search/', views.post_search, name='post_search'),
 ]
