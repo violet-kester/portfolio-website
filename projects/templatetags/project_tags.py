86630c7107fe348fb61644fefd00bda85b6d7a40
Violet Kester <kestervj89@gmail.com>
2024-03-19 16:43:53 -0400

    added multiply filter for reducing animation transition times in projects page

--- projects/templatetags/project_tags.py
+++ projects/templatetags/project_tags.py
@@ -6,10 +6,24 @@ register = template.Library()
 
 @register.inclusion_tag('projects/latest_projects.html')
 def get_latest_projects(count=3):
-    """Retrieves the latest `count` number of published projects."""
+    """
+    Retrieves the latest `count` number of published projects.
+    """
 
     latest_projects = Project.objects.filter(
         status='PB'
     ).order_by('-created')[:count]
 
-    return {'latest_projects': latest_projects }
+    return {'latest_projects': latest_projects}
+
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+    """
+    Multiplies the value by the arg.
+    Used to modify animation transition times.
+    """
+    try:
+        return value * arg
+    except (ValueError, TypeError):
+        return ''
